6cbdec5dbe3455515e0ab114ddfdf53bb66856fc
mohamednassirouahamadou9696@gmail.com <mohamednassirouahamadou9696@gmail.com>
2023-02-12 07:17:20 +0100

    JS01

---
+++ oddAndEven.js
@@ -0,0 +1,8 @@
+var i = 0;
+while (i <= 20) {
+  if(i % 2 == 0)
+    console.log('Paire');
+  if (i % 2 == 1)
+    console.log('Impaire');
+  i++;
+}
---
+++ weekendsVerified.js
@@ -0,0 +1,7 @@
+let Day;
+Day = 'lundi'
+if (Day === 'samedi' || Day === 'dimanche') {
+  console.log('weekend');
+} else {
+  console.log('weekday');
+}
